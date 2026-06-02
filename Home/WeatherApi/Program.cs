7e53b35f25553b4fd447539bcb60c0dbe7c37139
Marat <milya010790@mail.ru>
2026-06-02 10:32:08 +0500

    Решение второй части

--- Home/WeatherApi/Program.cs
+++ Home/WeatherApi/Program.cs
@@ -1,17 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ReactFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
-
+app.UseCors("ReactFrontend");
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
